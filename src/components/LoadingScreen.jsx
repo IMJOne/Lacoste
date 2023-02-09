b367343f04bcc751f3e538c5ed887ac621de8d63
IMJOne <mompapa7@gmail.com>
2023-02-10 08:55:36 +0900

    Edit product name

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import { Hearts } from 'react-loader-spinner';
 
 export default function LoadingScreen() {
   return (
-    <div className="bg-lightWhite absolute top-0 left-0 w-full h-full flex justify-center items-center">
+    <div className="bg-lightWhite fixed top-0 left-0 w-full h-full flex justify-center items-center">
       <Hearts color="#105a33" />
     </div>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -15,7 +15,7 @@ export default function Products() {
       {isLoading && <LoadingScreen />}
       {error && <NotFound />}
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
-        {products && products.map(product => <ProductCard key={product.id} product={product} />)}
+        {products && products.map((product, index) => <ProductCard key={index} product={product} />)}
       </ul>
     </main>
   );
